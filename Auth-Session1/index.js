076e0c4e959aea6426d6263c557bd56776470212
mrinmoyborah <mrinmoyborah26100@gmail.com>
2026-01-07 14:50:54 +0530

    session from scratch

--- Auth-Session1/index.js
+++ Auth-Session1/index.js
@@ -6,6 +6,7 @@ import userModel from "./model/userModel.js";
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcrypt";
+import crypto from "crypto";
 dotenv.config();
 
 const app = express();
@@ -19,9 +20,39 @@ mongoose.connect(process.env.MONGODB_URI)
         console.log("not connected to mongoDB");
     })
 
+//! SESSION STORAGE OF SERVER
+let sessions = {}
+
+function generateSessionId(){
+    return crypto.randomBytes(64).toString("hex");
+}
+
 //! MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+function authenticateSession(req, res, next){
+    const cookies = req.headers.cookie;
+    if(!cookies){
+        return res.json({msg: "login in to your account"});
+    }
+
+    const cookie = Object.fromEntries(
+        cookies.split("; ").map(c=>c.split("="))
+    );
+
+    if(sessions[cookieData.authSessionId]){
+        console.log("Session Id from cookie: ", sessions[cookie.sessionId]);
+    }else{
+        console.log("This session id is invalid");
+    }
+
+    const sessionId = cookie.sessionId;
+    if(sessionId && sessions[sessionId]){
+        req.sessionId = sessions[sessionId];
+    }
+
+}
 
 //! SIGNUP
 function isValidName(clientName){
@@ -66,7 +97,42 @@ app.post("/signup", async (req, res)=>{
     }
 })
 
+app.post("/login", async (req, res)=>{
+    let {email: userEmail, password: userPassword} = req.body;
 
+    if(!req.body || !userEmail || !userPassword){
+        return res.status(400).json({msg: "all fields are required"});
+    }
+
+    if(!validator.isEmail(userEmail)){
+        return res.status(400).json({msg: "invalid email"});
+    }
+    const existingUser = await userModel.findOne({emai: userEmail});
+    if(!existingUser){
+        return res.status(400).json({msg: "please login with a registered mail"});
+    }
+
+    const isPassword = await bcrypt.compare(userPassword, existingUser.password);
+    if(!isPassword){
+        return res.status(400).json({msg: "incorrect password"});
+    }
+
+    const sessionId = generateSessionId();
+        sessions[sessionId] = {
+            userId: existingUser._id,
+            email: existingUser.email,
+            createdAt: Date.now()
+        }
+    
+    res.cookie("authSessionId", sessionId, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 120 * 1000
+    })
+
+    return res.status(200).redirect("/test");
+})
 
 app.get("/test", (req, res)=>{
     res.status(200).json({msg: "hello!!! from server"});
