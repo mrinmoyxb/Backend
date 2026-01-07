344cd8e613f329bbc0f32f4f5eed85a6867b6fd3
mrinmoyborah <mrinmoyborah26100@gmail.com>
2026-01-07 08:54:05 +0530

    successful sign up

--- Auth-Session1/index.js
+++ Auth-Session1/index.js
@@ -1,46 +1,27 @@
-const express = require("express");
-const validator = require("validator");
-const userModel = require("./model/userModel");
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const cookieParser = require("cookie-parser");
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import userModel from "./model/userModel.js";
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcrypt";
+dotenv.config();
+
 const app = express();
-const PORT = 8005;
-require("dotenv").config();
+const PORT = process.env.PORT;
 
-mongoose.connect("mongodb://127.0.0.1:27017/auth")
+//! CONNECTION TO DATABASE
+mongoose.connect(process.env.MONGODB_URI)
     .then(()=>{
-        console.log("MongoDB is running");
-    })
-    .catch(()=>{
-        console.log("MongoDB is not running");
+        console.log("connected to mongoDB");
+    }).catch(()=>{
+        console.log("not connected to mongoDB");
     })
 
+//! MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-
-app.set("view engine", "ejs");
-
-//! JWT to authenticate route
-const secret_key = '2f66ddd7626733689c0672d04e94bcb87c113bc3bfe4955806f50ef4d137ae8f3d1b62e8562ece9374af2d1d0f4709f9d9808137cda99fefc0b2953ab232adaa';
-function authenticateJWT(req, res, next){
-    const JWTToken = req.cookies.JWTToken;
-    console.log("JWTToken: ", JWTToken);
-
-    if(!JWTToken){
-        return res.status(401).json({msg: "Login required"});
-    }
-    try{
-        const decoded = jwt.verify(JWTToken, secret_key); 
-        console.log("Decoded: ", decoded);
-        req.userId = decoded.userId;
-        next();
-    }catch(err){
-        return res.status(401).json({msg: "invalid or expired token"});
-    }
-}
 
 //! SIGNUP
 function isValidName(clientName){
@@ -48,95 +29,50 @@ function isValidName(clientName){
     return regex.test(clientName);
 }
 app.post("/signup", async (req, res)=>{
-    const body = req.body;
-    if(!body || !body.name || !body.email || !body.password || !body.code){
-        return res.status(400).json({msg: "all fields are required"});
-    }
-
-    if(!validator.isEmail(body.email)){
-        return res.status(400).json({msg: "enter a valid email"});
-    }
-
-    const existingUser = await userModel.findOne({email: body.email});
-    if(existingUser){
-        return res.status(400).json({msg: "this email is already registered"});
-    }
-
-    if(!isValidName(req.body.name)){
-        return res.status(400).json({msg: "invalid name"})
-    }
-
-    if(body.password.length < 8){
-        return res.status(400).json({msg: "the length of password is less than 8"});
-    }
-
-    const hashPassword = await bcrypt.hash(body.password, 10);
-    const newUser = await userModel.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashPassword,
-        code: req.body.code
-    });
-    console.log("newuser: ", newUser);
-    return res.status(200).json({msg: "welcome, user registered"});
-})
-
-//! LOGIN
-app.post("/login", async (req, res)=>{
     try{
-        const body = req.body;
-  
-        if(!body || !body.email || !body.password){
+        let {name: userName, email: userEmail, password: userPassword} = req.body;
+        userEmail = userEmail.toLowerCase();
+
+        if(!req.body || !userName  || !userEmail || !userPassword){
             return res.status(400).json({msg: "all fields are required"});
         }
 
-        if(!validator.isEmail(body.email.toLowerCase())){
+        if(!validator.isEmail(userEmail)){
             return res.status(400).json({msg: "invalid email format"});
         }
-
-        const existingUser = await userModel.findOne({email: body.email.toLowerCase()});
-        if(!existingUser){
-            return res.status(400).json({msg: "email not found"});
+        const existingUser = await userModel.findOne({email: userEmail});
+        if(existingUser){
+            return res.status(400).json({msg: "email is already registered"});
         }
 
-        const isPassMatch = await bcrypt.compare(body.password, existingUser.password);
-        if(!isPassMatch){
-            return res.status(401).json({msg: "wrong password, try again"});
+        if(!isValidName(userName)){
+            return res.status(400).json({msg: "invalid name"});
         }
 
-        //! JWT TOKEN AS COOKIE
-        const JWTToken = jwt.sign({userId: existingUser._id, email: existingUser.email},
-                                    secret_key,
-                                  {expiresIn: "1m"});
-        console.log("JWT Token created: ", JWTToken);
-        res.cookie("JWTToken", JWTToken, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "strict",
-            maxAge: 60 * 1000
+        if(userPassword.length < 8){
+            return res.status(400).json({msg: "length of password should be greater than or equal to 8"});
+        }
+        const hashedPassword = await bcrypt.hash(userPassword, Number(process.env.SALT_ROUNDS));
+
+        const newUser = await userModel.create({
+            name: userName,
+            email: userEmail,
+            password: hashedPassword
         })
 
-        return res.status(200).redirect("/home");
+        return res.status(201).json({msg: "user created successfully"});
     }catch(err){
-        return res.status(500).json("Internal server error");
+        return res.status(500).json({msg: "internal server error"});
     }
 })
 
-function getAppUrl(){
-     process.env.GOOGLE_CLIENT_ID
-}
 
-//! HOME
-app.get("/home", authenticateJWT, async (req, res)=>{
-    const userId = req.userId;
-    const user = await userModel.findById({_id: userId});
-    console.log("USER: ", user);
-    res.render("home", {
-        username: user.name,
-        code: user.code
-    });
-});
 
+app.get("/test", (req, res)=>{
+    res.status(200).json({msg: "hello!!! from server"});
+})
+
+//! CONNECTION TO SERVER
 app.listen(PORT, ()=>{
-    console.log("SERVER IS RUNNING ON PORT: ", PORT);
+    console.log("Server is running on the PORT: ", PORT);
 })
